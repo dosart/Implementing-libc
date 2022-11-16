6d00c0cc01fbda3c8c415f28e7a11a596b1d4626
dosart <Dysprosium92@ya.ru>
2022-11-16 22:41:58 +0300

    add comments

--- smart_pointers/shared_ptr.hpp
+++ smart_pointers/shared_ptr.hpp
@@ -1,3 +1,9 @@
+/*!
+@file
+@defgroup memory_management
+@brief  Header file for shared_ptr
+This file contains simple class shared_ptr.
+*/
 #ifndef MY_SHARED_PTR_HPP
 #define MY_SHARED_PTR_HPP
 
@@ -6,6 +12,11 @@
 
 namespace smart_ptr
 {
+    /**
+     * @ingroup memory_management
+     *
+     * @brief smart_ptr::shared_ptr is a smart pointer that retains shared ownership of an object through a pointer. Several shared_ptr objects may own the same object.
+     */
     template <typename T>
     class shared_ptr
     {
@@ -13,11 +24,25 @@ namespace smart_ptr
         using pointer = T *;
         using element_type = T;
 
-        /// Default constructor, creates a shared_ptr that owns nothing
+        /**
+         * @ingroup memory_management
+         *
+         * @brief Default constructor, creates a unique_ptr that owns nothing.
+         */
         constexpr shared_ptr() noexcept = default;
 
+        /**
+         * @ingroup memory_management
+         *
+         * @brief Constructor with std::nullptr_t, creates a unique_ptr that owns nothing.
+         */
         constexpr shared_ptr(std::nullptr_t) noexcept : m_data(nullptr), m_count(nullptr){};
 
+        /**
+         * @ingroup memory_management
+         *
+         * @brief  Constructs a shared_ptr with p as the pointer to the managed object.
+         */
         explicit shared_ptr(pointer data)
             : m_data(data), count(new (std::nothorw) int(1))
         {
@@ -28,6 +53,11 @@ namespace smart_ptr
             }
         }
 
+        /**
+         * @ingroup memory_management
+         *
+         * @brief  Destructs the owned object if no more shared_ptrs link to it.
+         */
         ~shared_ptr()
         {
             --(*m_count);
@@ -43,6 +73,11 @@ namespace smart_ptr
             ++(*m_count);
         }
 
+        /**
+         * @ingroup memory_management
+         *
+         * @brief  Replaces the managed object with the one managed by src.
+         */
         shared_ptr &operator=(unique_ptr &src)
         {
             shared_ptr tmp(src);
@@ -50,7 +85,13 @@ namespace smart_ptr
             return *this;
         }
 
-        void swap(shared_ptr &src) noexcept
+        /**
+         * @ingroup memory_management
+         *
+         * @brief  Specializes the std::swap algorithm.
+         */
+        void
+        swap(shared_ptr &src) noexcept
         {
             std::swap(m_data, src.m_data);
             std::swap(m_count, src.m_count);
@@ -60,11 +101,37 @@ namespace smart_ptr
         pointer operator->() const noexcept { return m_data; }
         element_type &operator*() const { return *m_data; }
 
-        /// Access to smart pointer state
-        pointer get() const noexcept { return m_data; }
+        /**
+         * @ingroup memory_management
+         *
+         * @brief  Returns the stored pointer.
+         *
+         * @returns The stored pointer.
+         */
+        pointer
+        get() const noexcept
+        {
+            return m_data;
+        }
+
+        /**
+         * @ingroup memory_management
+         *
+         * @brief checks if the stored pointer is not null.
+         *
+         * @returns True if *this stores a pointer, false otherwise..
+         */
         explicit operator bool() const noexcept { return m_data; }
 
-        size_t use_count() const noexcept
+        /**
+         * @ingroup memory_management
+         *
+         * @brief  Returns the number of shared_ptr objects referring to the same managed object.
+         *
+         * @returns The number of smart_pointer::shared_ptr instances managing the current object or ​0​ if there is no managed object.
+         */
+        size_t
+        use_count() const noexcept
         {
             return m_count != nullptr ? *m_count : 0;
         }
--- smart_pointers/unique_ptr.hpp
+++ smart_pointers/unique_ptr.hpp
@@ -2,7 +2,7 @@
 @file
 @defgroup memory_management
 @brief  Header file for unique_ptr
-This file contains simple class unique_ptr. 
+This file contains simple class unique_ptr.
 */
 
 #ifndef MY_UNIQUE_PTR_HPP
@@ -14,10 +14,10 @@ This file contains simple class unique_ptr.
 namespace smart_ptr
 {
     /**
-    * @ingroup memory_management
-    *
-    * @brief smart_ptr::unique_ptr is a smart pointer that owns and manages another object through a pointer and disposes of that object when the unique_ptr goes out of scope..
-    */
+     * @ingroup memory_management
+     *
+     * @brief smart_ptr::unique_ptr is a smart pointer that owns and manages another object through a pointer and disposes of that object when the unique_ptr goes out of scope..
+     */
     template <typename T>
     class unique_ptr
     {
@@ -26,24 +26,24 @@ namespace smart_ptr
         using element_type = T;
 
         /**
-        * @ingroup memory_management
-        *
-        * @brief Default constructor, creates a unique_ptr that owns nothing.
-        */
+         * @ingroup memory_management
+         *
+         * @brief Default constructor, creates a unique_ptr that owns nothing.
+         */
         constexpr unique_ptr() noexcept = default;
 
         /**
-        * @ingroup memory_management
-        *
-        * @brief Constructor with std::nullptr_t, creates a unique_ptr that owns nothing.
-        */
+         * @ingroup memory_management
+         *
+         * @brief Constructor with std::nullptr_t, creates a unique_ptr that owns nothing.
+         */
         constexpr unique_ptr(std::nullptr_t) noexcept : m_data(nullptr){};
 
         /**
-        * @ingroup memory_management
-        *
-        * @brief Constructor with pointer, creates a unique_ptr that owns pointer.
-        */
+         * @ingroup memory_management
+         *
+         * @brief Constructor with pointer, creates a unique_ptr that owns pointer.
+         */
         explicit unique_ptr(pointer p) noexcept : m_data(p) {}
 
         /// Disables copy constructor
@@ -53,10 +53,10 @@ namespace smart_ptr
         unique_ptr &operator=(const unique_ptr &) = delete;
 
         /**
-        * @ingroup memory_management
-        *
-        * @brief Destructor. Remove memory if pointer isn't nullptr.
-        */
+         * @ingroup memory_management
+         *
+         * @brief Destructor. Remove memory if pointer isn't nullptr.
+         */
         ~unique_ptr() noexcept
         {
             if (m_data)
@@ -64,48 +64,48 @@ namespace smart_ptr
         }
 
         /**
-        * @ingroup memory_management
-        *
-        * @brief Access owned object.
-        *
-        * @returns Returns owned object.
-        */
+         * @ingroup memory_management
+         *
+         * @brief Access owned object.
+         *
+         * @returns Returns owned object.
+         */
         pointer operator->() const { return m_data; }
-        
+
         /**
-        * @ingroup memory_management
-        *
-        * @brief Access owned object.
-        *
-        * @returns Returns owned object.
-        */
+         * @ingroup memory_management
+         *
+         * @brief Access owned object.
+         *
+         * @returns Returns owned object.
+         */
         element_type &operator*() const { return *m_data; }
-        
+
         /**
-        * @ingroup memory_management
-        *
-        * @brief Access to smart pointer state.
-        *
-        * @returns Returns a pointer to the managed object.
-        */
+         * @ingroup memory_management
+         *
+         * @brief Access to smart pointer state.
+         *
+         * @returns Returns a pointer to the managed object.
+         */
         pointer get() const { return m_data; }
-        
+
         /**
-        * @ingroup memory_management
-        *
-        * @brief Access to smart pointer state.
-        *
-        * @returns Returns smart pointer state.
-        */
+         * @ingroup memory_management
+         *
+         * @brief Checks if the unique pointer is not null.
+         *
+         * @returns True if *this stores a pointer, false otherwise..
+         */
         explicit operator bool() const { return m_data; }
 
         /**
-        * @ingroup memory_management
-        *
-        * @brief Access to smart pointer state.
-        *
-        * @returns Returns a pointer to the managed object and releases the ownership.
-        */
+         * @ingroup memory_management
+         *
+         * @brief Access to smart pointer state.
+         *
+         * @returns Returns a pointer to the managed object and releases the ownership.
+         */
         pointer release()
         {
             T *result = nullptr;
@@ -113,7 +113,6 @@ namespace smart_ptr
             return result;
         }
 
-        
         void swap(unique_ptr &src) noexcept
         {
             std::swap(m_data, src.m_data);
@@ -123,7 +122,7 @@ namespace smart_ptr
         {
             moving.swap(*this);
         }
-        
+
         unique_ptr &operator=(unique_ptr &&moving) noexcept
         {
             moving.swap(*this);
